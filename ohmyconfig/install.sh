16a11c1c917e4f11b3d389e5d87047d94cd8ea8b
iamtieqi <a13603205187@163.com>
2026-06-20 21:45:42 +0800

    0003

---
+++ ohmyconfig/install.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+tar xvf cascadia-code.tar.gz
+
+sudo cp -r cascadia-code /usr/share/fonts/
+
+sudo fc-cache -fv
+
+sudo apt install i3 rofi compton feh mate-terminal
+
+cp ./Pictures ~/
+
+cp spacemacs ~/.spacemacs
+
+
+git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+
+
+
+
